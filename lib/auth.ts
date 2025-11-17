cd9debc64d59bc734e9878d770abdd4686d28cc4
7minhauser <7minhauser@gmail.com>
2025-11-17 11:40:32 +0900

    Deploy issue

--- lib/auth.ts
+++ lib/auth.ts
@@ -14,6 +14,7 @@ export const authOptions: NextAuthOptions = {
   pages: {
     signIn: '/auth/signin',
   },
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async session({ session, user }) {
       if (session.user) {
